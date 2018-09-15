8dabfb478abdef60c0366914b8a9e63a0f0b5d02
EC2 Default User <ec2-user@ip-172-31-25-239.us-east-2.compute.internal>
2018-09-15 08:06:10 +0000

    finish making sessions/new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    @host = Host.find_by(params[:user])
+    if @host
+      redirect_to("/hosts/#{@host.id}")
+    else
+      render("sessions/new")
+    end
+  end
+
 end
