0755881009ff5f830de6c134b3508f76f4b6e8c5
Tomoko Ohtsuka <kinacomochico.821@gmail.com>
2018-10-22 08:13:33 +0000

    made login information for guests

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   before_action :login_as_host_or_guest, only: [:show]
   before_action :logged_in_host,  only: [:new, :create, :index, :destroy]
+  before_action :forbid_logged_in_host,  only:[:guest_login_view, :guest_login]
   before_action :forbid_logged_in_guest, only:[:guest_login_view, :guest_login]
   
   def new
@@ -37,7 +38,8 @@ class EventsController < ApplicationController
   
   def guest_login_view # (GET)
     # ログイン画面をだす(newアクション的な役割)
-    @event_id = current_event.id
+    photo = Photo.find_by(id: params[:id])
+    @event_id = current_event.id || photo.event_id
   end
   
   def guest_login # (POST:create的な役割)
@@ -45,7 +47,7 @@ class EventsController < ApplicationController
     if params[:event][:guest_password] == current_event.guest_password
     #    sessionに合言葉を入れる
       session[:guest_password] = params[:event][:guest_password]
-      redirect_to event_path(current_event), flash: { success: "イベントにログインしました" }
+      redirect_to event_path(current_event), flash: { success: "ログインしました" }
     else
       flash.now[:danger] = "ゲストパスワードを入力してください"
       render :guest_login_view
@@ -54,7 +56,7 @@ class EventsController < ApplicationController
   
   def guest_logout
     session[:guest_password] = nil
-    redirect_to login_event_path, flash: { success: "ゲストログアウトしました" }
+    redirect_to login_event_path, flash: { success: "ログアウトしました" }
   end
   
   def destroy
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -7,13 +7,16 @@ module EventsHelper
     @event_id = params[:id] || params.require(:photo).permit(:event_id)[:event_id]
     #params.require(:photo).permit(:event_id)の結果が、
     # { event_id: 1 }というハッシュで返ってくるから、値をとるときは[:event_id]をつける必要がある
-    @current_event ||= Event.find_by(id: @event_id)
+    photo = Photo.find_by(id: @event_id)
+    @current_event ||= Event.find_by(id: @event_id) || photo.event_id
+                                                    #events/showでlogoutならcurrent_eventで持ってこれるけど
+                                                    #photos/index画面でlogoutするとevent_id持ってこれないから
+                                                    #その場合はphotoに紐づいたイベントIDをもってくるようにする
   end
   
   def guest_logged_in?
       session[:guest_password].present?
       # sessionが入っているか(セッションの中身は合言葉でOK)
       # sessionsヘルパー
-    #end
   end
 end
