d1631cda69de876b5444e898ad3370256365bbf3
Tomoko Ohtsuka <kinacomochico.821@gmail.com>
2018-10-23 02:52:06 +0000

    readjust css

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,6 @@ class Event < ApplicationRecord
   belongs_to :host
   has_many :photos
   validates :title, {presence: true}
+  validates :description, {length: {maximum: 30}}
   validates :guest_password, {presence: true, length: {mininum: 6, maximum: 20}}
 end
